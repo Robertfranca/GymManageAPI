28a49a1c2c6dd840b34175bd0fba261447ea0638
robert-081 <robertalan.dsf@gmail.com>
2022-06-26 16:18:45 -0300

    Aprimorando os atributos do Model da entidade Aluno.

--- src/main/java/com/gymManage/modules/AlunoCadModels.java
+++ src/main/java/com/gymManage/modules/AlunoCadModels.java
@@ -17,35 +17,35 @@ public class AlunoCadModels {
     @Column(name = "nome", length = 100)
     private String nome;
 
-    @Column(name = "sobrenome", length = 50)
+    @Column(name = "sobrenome", length = 100)
     private String sobrenome;
 
-//    @Column(name = "email", length = 100)
-//    private String email;
-//
-//    @Column(name = "cpf", length = 255)
-//    private String cpf;
-//
-//    @Column(name = "telefone", length = 100)
-//    private String telefone;
-//
-//    @Column(name = "peso", length = 100)
-//    private String peso;
-//
-//    @Column(name = "altura", length = 100)
-//    private String altura;
-//
+    @Column(name = "email", length = 100)
+    private String email;
+
+    @Column(name = "cpf", length = 100)
+    private String cpf;
+
+    @Column(name = "telefone", length = 100)
+    private String telefone;
+
+    @Column(name = "peso", length = 100)
+    private String peso;
+
+    @Column(name = "altura", length = 100)
+    private String altura;
+
 //    @Column(name = "dropvalue")
 //    private String dropvalue;
 //
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public void setId(long id) {
-//        this.id = id;
-//    }
-//
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getNome() {
         return nome;
     }
@@ -62,47 +62,47 @@ public class AlunoCadModels {
         this.sobrenome = sobrenome;
     }
 
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getCpf() {
-//        return cpf;
-//    }
-//
-//    public void setCpf(String cpf) {
-//        this.cpf = cpf;
-//    }
-//
-//    public String getTelefone() {
-//        return telefone;
-//    }
-//
-//    public void setTelefone(String telefone) {
-//        this.telefone = telefone;
-//    }
-//
-//    public String getPeso() {
-//        return peso;
-//    }
-//
-//    public void setPeso(String peso) {
-//        this.peso = peso;
-//    }
-//
-//    public String getAltura() {
-//        return altura;
-//    }
-//
-//    public void setAltura(String altura) {
-//        this.altura = altura;
-//    }
-//
-//
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+
 //    public String getDropvalue() {
 //        return dropvalue;
 //    }
